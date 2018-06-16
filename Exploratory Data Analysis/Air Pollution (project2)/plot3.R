fe5de4c46c820612c5d9507398a0e8bfdd157caf
Shekhar <dzzle.shekhar007@outlook.com>
2018-06-17 02:14:54 +0530

    initial commit

---
+++ Exploratory Data Analysis/Air Pollution (project2)/plot3.R
@@ -0,0 +1,25 @@
+## This first line will likely take a few seconds. Be patient!
+if(!exists("NEI")){
+  NEI <- readRDS("summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("Source_Classification_Code.rds")
+}
+
+library(ggplot2)
+
+
+
+# Subset NEI data by Baltimore 24510 
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+g3<-ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+print(g3)
+dev.off()
